37269a8e6b61f4cce987f22ae5f72b69a4c4cf47
ClickerOfThings <Prikolist>
2022-07-15 13:20:19 +0400

    (уже) ненужные интерфейсы

--- OverpassClasses.cs
+++ OverpassClasses.cs
@@ -9,7 +9,7 @@ namespace OverpassLibrary
     /// <summary>
     /// Объект OSM
     /// </summary>
-    public class OsmClass : IEquatable<OsmClass>, IComparable<OsmClass>
+    public class OsmClass
     {
         /// <summary>
         /// ID объекта с литерой типа ([N]ode, [W]ay, [R]elation) в начале
@@ -70,54 +70,5 @@ namespace OverpassLibrary
             !string.IsNullOrEmpty(City) &&
             !string.IsNullOrEmpty(Street) &&
             !string.IsNullOrEmpty(HouseNum);
-        /// <summary>
-        /// Проверяет, равен ли OsmId текущего объекта OsmId проверяемого объекта
-        /// </summary>
-        /// <param name="other">Проверяемый объект</param>
-        /// <returns>true если OsmId у обоих объектов равны, false если не равны</returns>
-        public bool Equals([AllowNull] OsmClass other)
-        {
-            if (other is null)
-                return false;
-            if (this.OsmId == other.OsmId)
-                return true;
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            return OsmId.GetHashCode();
-        }
-        /// <summary>
-        /// Проверяет, равен ли OsmId текущего объекта OsmId проверяемого объекта
-        /// </summary>
-        /// <param name="obj">Проверяемый объект</param>
-        /// <returns>true если OsmId у обоих объектов равны, false если не равны</returns>
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as OsmClass);
-        }
-        /// <summary>
-        /// Сравнение проверяемого OSM объекта с текущим по OsmId, включая литеру в начале
-        /// </summary>
-        /// <param name="other">Проверяемый объект</param>
-        /// <returns>-1 если OsmId текущего объекта меньше OsmId проверяемого объекта,
-        /// 0 если OsmId текущего объекта равен OsmId проверяемого объекта,
-        /// 1 если OsmId текущего объекта больше OsmId проверяемого объекта</returns>
-        public int CompareTo([AllowNull] OsmClass other)
-        {
-            char xNodeTypeChar = this.OsmId[0];
-            char yNodeTypeChar = other.OsmId[0];
-            if (xNodeTypeChar != yNodeTypeChar)
-                return xNodeTypeChar.CompareTo(yNodeTypeChar);
-            long xID = long.Parse(this.OsmId.Substring(1));
-            long yID = long.Parse(other.OsmId.Substring(1));
-            if (xID > yID)
-                return 1;
-            if (xID < yID)
-                return -1;
-            else
-                return 0;
-        }
     }
 }
