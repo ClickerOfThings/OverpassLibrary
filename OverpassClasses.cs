705d144d719d4243e74a7345e5c783e58ff337b2
ClickerOfThings <Prikolist>
2022-07-15 08:57:08 +0400

    два интерфейса в классе, переделаны и переписаны методы

--- OverpassClasses.cs
+++ OverpassClasses.cs
@@ -1,55 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Text;
 
 namespace OverpassLibrary
 {
-    public enum OsmObjTypes
+    public class OsmClass : IEquatable<OsmClass>, IComparable<OsmClass>
     {
-        Node,
-        Way,
-        Relation
-    }
-    public class osmClass
-    {
-        public static Dictionary<OsmObjTypes, char> ObjTypeToChar = new Dictionary<OsmObjTypes, char>()
-        {
-            {OsmObjTypes.Node, 'N' },
-            {OsmObjTypes.Way, 'W' },
-            {OsmObjTypes.Relation, 'R' }
-        };
-        public static Dictionary<char, OsmObjTypes> CharToObjType = new Dictionary<char, OsmObjTypes>()
-        {
-            {'N', OsmObjTypes.Node },
-            {'W', OsmObjTypes.Way },
-            {'R', OsmObjTypes.Relation }
-        };
-
-        public long Id { get; set; } = -1;
-        public OsmObjTypes ObjType { get; set; }
+        public string OsmId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string PlaceType { get; set; }
         public string Name { get; set; }
         public string State { get; set; }
         public string City { get; set; }
-        public string CityId { get; set; }
         public PointF CitySouthWest { get; set; }
         public PointF CityNorthEast { get; set; }
         public string Street { get; set; }
         public string HouseNum { get; set; }
-        public string FullAddress =>
+        public string FullAddressString =>
             (string.IsNullOrEmpty(PostCode) ? "" : PostCode + ", ") 
             + State + ", " + City + " г, " +
             Street + ", " + HouseNum;
         public string PostCode { get; set; }
-
-        public bool IsAddressFull(bool checkPostCode) =>
-            (!checkPostCode || !string.IsNullOrEmpty(PostCode)) &&
+        public bool IsAddressFull =>
             !string.IsNullOrEmpty(State) &&
             !string.IsNullOrEmpty(City) &&
             !string.IsNullOrEmpty(Street) &&
             !string.IsNullOrEmpty(HouseNum);
+
+        public int Compare(OsmClass x, OsmClass y)
+        {
+            long xID = long.Parse(x.OsmId.Substring(1));
+            long yID = long.Parse(y.OsmId.Substring(1));
+            if (xID > yID)
+                return 1;
+            if (xID < yID)
+                return -1;
+            else
+                return 0;
+
+        }
+
+        public bool Equals([AllowNull] OsmClass other)
+        {
+            if (other is null)
+                return false;
+            if (this.OsmId == other.OsmId)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return OsmId.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as OsmClass);
+        }
+
+        public int CompareTo([AllowNull] OsmClass other)
+        {
+            char xNodeTypeChar = this.OsmId[0];
+            char yNodeTypeChar = other.OsmId[0];
+            if (xNodeTypeChar != yNodeTypeChar)
+                return xNodeTypeChar.CompareTo(yNodeTypeChar);
+            long xID = long.Parse(this.OsmId.Substring(1));
+            long yID = long.Parse(other.OsmId.Substring(1));
+            if (xID > yID)
+                return 1;
+            if (xID < yID)
+                return -1;
+            else
+                return 0;
+        }
     }
 }
--- OverpassMethods.cs
+++ OverpassMethods.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
@@ -12,169 +13,183 @@ namespace OverpassLibrary
 {
     public static class OverpassMethods
     {
-        /// <returns>Поскольку могут быть города с одинаковыми названиями, 
-        /// метод возвращает массив объектов городов.</returns>
-        public static List<osmClass> GetCityInfo(string cityNameInRussian)
+        /// <summary>
+        /// Возвращает информацию о городах из Nominatim API
+        /// </summary>
+        /// <param name="cityNameInRussian">Название города на русском</param>
+        /// <returns>Объект класса OsmClass с заполненной информацией о названии, области и границах прямоугольника</returns>
+        /// <exception cref="ArgumentException">Город не может быть найден в базе данных OSM, 
+        /// либо найденный город не имеет необходимого OSM типа relation</exception>
+        /// <remarks>Поскольку возможны города с одинаковыми именами, метод возвращает список найденных городов, 
+        /// а не отдельный город. Чтобы отличать такие города, следует использовать свойство <code>State</code>, 
+        /// которое хранит в себе область города</remarks>
+        public static List<OsmClass> GetCityInfo(string cityNameInRussian)
         {
             WebRequest reqForCity = WebRequest.Create($"https://nominatim.openstreetmap.org/search" +
                 $"?city={cityNameInRussian}" +
                 $"&format=json" +
                 $"&addressdetails=1" +
                 $"&accept-language=ru-RU");
-            reqForCity.Headers.Add(HttpRequestHeader.UserAgent, "c# script");
-            List<osmClass> returnObjects = new List<osmClass>();
-            dynamic mainObj;
+            reqForCity.Headers.Add(HttpRequestHeader.UserAgent, "dotnet");
+            List<OsmClass> foundCities = new List<OsmClass>();
+            dynamic cityJArray;
             using (StreamReader reader = new StreamReader(reqForCity.GetResponse().GetResponseStream()))
             {
-                mainObj = JArray.Parse(reader.ReadToEnd());
+                cityJArray = JArray.Parse(reader.ReadToEnd());
             }
-            if (mainObj.Count == 0)
-                throw new ArgumentException("Города не существует");
-            foreach (dynamic city in mainObj)
+            if (cityJArray.Count == 0)
+                //throw new ArgumentException("Города не существует");
+                return foundCities;
+
+            foreach (dynamic city in cityJArray)
             {
                 if (city.osm_type != "relation")
                     continue;
-                osmClass newObj = new osmClass();
-                newObj.City = city.address.city ?? city.address.town ?? city.address.village;
-                newObj.CityId = city.osm_id;
-                newObj.State = city.address.state;
-                dynamic boxArray = city.boundingbox;
-                newObj.CityNorthEast = new System.Drawing.PointF(float.Parse(boxArray[1].Value), float.Parse(boxArray[3].Value));
-                newObj.CitySouthWest = new System.Drawing.PointF(float.Parse(boxArray[0].Value), float.Parse(boxArray[2].Value));
-                returnObjects.Add(newObj);
+                OsmClass newCity = new OsmClass();
+                newCity.City = city.address.city ?? city.address.town ?? city.address.village;
+                newCity.State = city.address.state;
+                dynamic cityBoundingBox = city.boundingbox;
+                newCity.CityNorthEast = new System.Drawing.PointF(
+                    float.Parse(cityBoundingBox[1].Value),
+                    float.Parse(cityBoundingBox[3].Value));
+                newCity.CitySouthWest = new System.Drawing.PointF(
+                    float.Parse(cityBoundingBox[0].Value),
+                    float.Parse(cityBoundingBox[2].Value));
+                foundCities.Add(newCity);
             }
-            if (returnObjects.Count == 0)
+            /*if (foundCities.Count == 0)
                 throw new ArgumentException("Ошибка поиска города. Города не существует, либо для города нет типа relation " +
-                    "в базе данных OSM");
-            return returnObjects;
+                    "в базе данных OSM");*/
+            return foundCities;
         }
-
-        public static List<osmClass> GetAllPlacesInBox(PointF northEast, PointF southWest, params string[] placeTypes)
+        /// <summary>
+        /// Найти все OSM точки в прямоугольнике из базы данных openstreetmap.ru
+        /// </summary>
+        /// <param name="northEastPoint">Северо-восточный угол прямоугольника</param>
+        /// <param name="southWestPoint">Юго-западный угол прямоугольника</param>
+        /// <param name="placeTypes">Типы точек, которые необходимо найти</param>
+        /// <returns>Список объектов класса OsmClass, представляющий точки, найденные в прямоугольнике</returns>
+        /// <exception cref="ArgumentException">Не найдены точки (пустой запрос от сервера)</exception>
+        public static List<OsmClass> GetAllPlacesInBox(PointF northEastPoint, PointF southWestPoint, params string[] placeTypes)
         {
-            return GetAllPlacesInCity(new osmClass
-            {
-                CityNorthEast = northEast,
-                CitySouthWest = southWest
-            }, placeTypes);
-        }
-        public static List<osmClass> GetAllPlacesInCity(osmClass cityInfo, params string[] placeTypes)
-        {
-            List<osmClass> places = new List<osmClass>();
+            List<OsmClass> places = new List<OsmClass>();
             StringBuilder urlBuilder = new StringBuilder($"https://openstreetmap.ru/api/poi" +
                 $"?action=getpoibbox" +
-                $"&t={cityInfo.CityNorthEast.X}&r={cityInfo.CityNorthEast.Y}" +
-                $"&b={cityInfo.CitySouthWest.X}&l={cityInfo.CitySouthWest.Y}" +
+                $"&t={northEastPoint.X}&r={northEastPoint.Y}" +
+                $"&b={southWestPoint.X}&l={southWestPoint.Y}" +
                 $"&nclass=");
-            for (int i = 0; i < placeTypes.Length; i++)
+
+            string lastPlaceInArray = placeTypes.Last();
+            foreach (string placeType in placeTypes)
             {
-                urlBuilder.Append(placeTypes[i]);
-                if (i < placeTypes.Length - 1)
+                urlBuilder.Append(placeType);
+                if (placeType != lastPlaceInArray)
                     urlBuilder.Append(",");
             }
 
-            WebRequest request = WebRequest.Create(urlBuilder.ToString());
-            dynamic mainObj;
-            using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
+            WebRequest placesReq = WebRequest.Create(urlBuilder.ToString());
+            dynamic placesJArray;
+            using (StreamReader reader = new StreamReader(placesReq.GetResponse().GetResponseStream()))
             {
                 try
                 {
-                    mainObj = JObject.Parse(reader.ReadToEnd());
+                    JObject parsedResponse = JObject.Parse(reader.ReadToEnd());
+                    if (!parsedResponse.ContainsKey("data"))
+                        return places;
+                    placesJArray = parsedResponse["data"];
                 }
-                catch(JsonReaderException ex)
+                catch (JsonReaderException ex)
                 {
-                    throw new ArgumentException("Города не существует, либо не найдено точек с введёнными типами");
+                    throw new ArgumentException("Во время обработки запроса о точках произошла ошибка: " + ex.Message);
                 }
             }
-            if (mainObj.data.Count == 0)
-                throw new ArgumentException("Объектов с введёнными типами не существует");
-            dynamic placesObjs = mainObj.data;
-            foreach (dynamic placeObj in placesObjs)
+            if (placesJArray.Count == 0)
+                return places;
+
+            foreach (dynamic place in placesJArray)
             {
-                if (string.IsNullOrEmpty(placeObj.addr_street.Value) ||
-                    string.IsNullOrEmpty(placeObj.addr_house.Value) ||
-                    string.IsNullOrEmpty(placeObj.name_ru.Value))
+                if (string.IsNullOrEmpty(place.addr_street.Value) ||
+                    string.IsNullOrEmpty(place.addr_house.Value) ||
+                    string.IsNullOrEmpty(place.name_ru.Value))
                     continue;
-                osmClass newObj = new osmClass()
+                OsmClass newObj = new OsmClass()
                 {
-                    Id = long.Parse(placeObj.osm_id.Value.Substring(1)),
-                    ObjType = osmClass.CharToObjType[placeObj.osm_id.Value.ToUpper()[0]],
-                    PlaceType = placeObj["class"],
-                    Name = placeObj.name_ru,
-                    City = placeObj.addr_city.Value ?? placeObj.addr_village,
-                    State = placeObj.addr_region,
-                    Latitude = placeObj.lat,
-                    Longitude = placeObj.lon,
-                    Street = placeObj.addr_street,
-                    HouseNum = placeObj.addr_house
+                    OsmId = ((string)place.osm_id).ToUpper(), // N, W или R в начале id должны быть заглавными для
+                                                              // дальнейшей работы с Nominatim API
+                    Name = place.name_ru,
+                    City = place.addr_city ?? place.addr_village,
+                    State = place.addr_region,
+                    Latitude = place.lat,
+                    Longitude = place.lon,
+                    Street = place.addr_street,
+                    HouseNum = place.addr_house
                 };
                 places.Add(newObj);
             }
-            SetPostCodes(ref places);
-            places.RemoveAll(x => !x.IsAddressFull(false)); // убираем места с неполными адресами
+            List<OsmClass> objectsWithoutPostcodes = places.Where(x => string.IsNullOrEmpty(x.PostCode)).ToList();
+            if (objectsWithoutPostcodes.Count != 0)
+                SetPostCodes(ref objectsWithoutPostcodes);
+            places.RemoveAll(x => !x.IsAddressFull); // убираем места с неполными адресами
             return places;
         }
-
-        public static string LookUpPostCode(osmClass objToLookUp)
+        const int MAX_IDS_PER_POSTCODE_REQUEST = 50;
+        /// <summary>
+        /// Установить почтовые индексы для точек, если имеются, из Nominatim API
+        /// </summary>
+        /// <param name="objectsToLookUp">Список точек, для которых необходимо найти почтовые индексы</param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void SetPostCodes(ref List<OsmClass> objectsToLookUp)
         {
-            if (objToLookUp.Id < 0)
-                throw new ArgumentException("Пустой объект класса osmClass в методе LookUpPostCode");
-            WebRequest request = WebRequest.Create($"https://nominatim.openstreetmap.org/lookup" +
-                $"?osm_ids={osmClass.ObjTypeToChar[objToLookUp.ObjType] + objToLookUp.Id.ToString()}" +
-                $"&format=json" +
-                $"&accept-language=ru-RU");
-            request.Headers.Add(HttpRequestHeader.UserAgent, "c# script");
-            dynamic lookUpObj;
-            using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
-            {
-                lookUpObj = JArray.Parse(reader.ReadToEnd());
-            }
-            if (lookUpObj.Count == 0)
-                throw new ArgumentException("Пустой ответ от API. Вероятнее всего, неправильный ID или " +
-                    "тип объекта");
-            return lookUpObj[0].address.postcode;
-        }
-
-        public static void SetPostCodes(ref List<osmClass> objectsToLookUp)
-        {
-            int objectCounter = 0;
-            while (objectCounter < objectsToLookUp.Count)
+            int currentEndIndex = 0;
+            for (int currentIndex = 0; currentIndex < objectsToLookUp.Count;)
             {
                 StringBuilder requestUrlBuilder = new StringBuilder($"https://nominatim.openstreetmap.org/lookup" +
                     $"?format=json" +
                     $"&accept-language=ru-RU" +
                     $"&osm_ids=");
-                int previousObjectCounter = objectCounter;
-                int currentMax = Math.Clamp(objectCounter + 50, int.MinValue, objectsToLookUp.Count);
-                for (; objectCounter < currentMax; objectCounter++)
+                currentEndIndex = Math.Clamp(currentIndex + MAX_IDS_PER_POSTCODE_REQUEST, 0, objectsToLookUp.Count);
+                for (; currentIndex < currentEndIndex; currentIndex++)
                 {
-                    if (objectsToLookUp[objectCounter].Id < 0)
-                        throw new ArgumentException("Пустой объект класса osmClass в методе LookUpPostCode: " +
-                            objectsToLookUp[objectCounter].FullAddress);
-                    requestUrlBuilder.Append(
-                        osmClass.ObjTypeToChar[objectsToLookUp[objectCounter].ObjType] + objectsToLookUp[objectCounter].Id.ToString());
-                    if (objectCounter < currentMax - 1)
+                    requestUrlBuilder.Append(objectsToLookUp[currentIndex].OsmId);
+                    if (objectsToLookUp[currentIndex] != objectsToLookUp[currentEndIndex - 1])
                         requestUrlBuilder.Append(',');
                 }
                 Thread.Sleep(1000); // снижение нагрузки на nominatim сервер для избежания 429
                 // если нужно рискнуть - комментируем строку или ставим таймаут меньше :)
                 WebRequest request = WebRequest.Create(requestUrlBuilder.ToString());
-                request.Headers.Add(HttpRequestHeader.UserAgent, "c# script");
-                dynamic lookUpObj;
+                request.Headers.Add(HttpRequestHeader.UserAgent, "dotnet");
+                List<OsmClass> parsedObjsWithPostcodesFound = new List<OsmClass>(50);
                 using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
                 {
-                    lookUpObj = JArray.Parse(reader.ReadToEnd());
-                }
-                if (lookUpObj.Count == 0)
-                    throw new ArgumentException("Пустой ответ от API. Вероятнее всего, неправильный ID или " +
-                        "тип объекта");
-                for (int i = 0; i < lookUpObj.Count; previousObjectCounter++, i++)
-                {
-                    if (lookUpObj[i].osm_id != objectsToLookUp[previousObjectCounter].Id)
+                    JArray parsedObjects = JArray.Parse(reader.ReadToEnd());
+                    foreach (dynamic obj in parsedObjects)
                     {
-                        i--;
-                        continue;
+                        if (obj?.address?.postcode is null)
+                            continue;
+                        string newOsmId = ((string)obj.osm_type).ToUpper()[0] + (string)obj.osm_id;
+                        parsedObjsWithPostcodesFound.Add(new OsmClass
+                        {
+                            OsmId = newOsmId,
+                            PostCode = obj.address.postcode
+                        });
                     }
-                    objectsToLookUp[previousObjectCounter].PostCode = lookUpObj[i].address.postcode;
+                }
+                if (parsedObjsWithPostcodesFound.Count == 0)
+                    /*throw new ArgumentException("Пустой ответ от API. Вероятнее всего, неправильный ID или " +
+                        "тип объекта");*/ // наверное, довольно радикально на каждый пустой запрос возвращать исключение
+                    continue;
+                List<OsmClass> lookUpObjsWithPostcodesFound =
+                    objectsToLookUp.Intersect(parsedObjsWithPostcodesFound).ToList();
+                parsedObjsWithPostcodesFound.Sort(); // для двоичного поиска
+                for (int i = 0; i < lookUpObjsWithPostcodesFound.Count; i++)
+                {
+                    OsmClass parsedObjWithSameId = parsedObjsWithPostcodesFound[
+                        parsedObjsWithPostcodesFound.BinarySearch(new OsmClass
+                        {
+                            OsmId = lookUpObjsWithPostcodesFound[i].OsmId
+                        })];
+                    lookUpObjsWithPostcodesFound[i].PostCode =
+                        parsedObjWithSameId.PostCode;
                 }
             }
         }
